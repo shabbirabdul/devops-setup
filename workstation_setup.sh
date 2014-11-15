a23751ba814423470c2c1f58adfaa251233e60f6
devops-ncsu <sabdu@ncsu.edu>
2014-11-15 17:36:55 -0500

    Update workstation_setup.sh
    scp command to transfer certificates

--- workstation_setup.sh
+++ workstation_setup.sh
@@ -8,4 +8,6 @@ rm -rf ~/.chef
 cd ~/.chef
 scp username@chef-server:/etc/chef-server/admin.pem ~/.chef
 scp username@chef-server:/etc/chef-server/chef-validator.pem ~/.chef
+scp username@chef-server:var/opt/chef-server/nginx/ca/SERVER_HOSTNAME.crt /etc/chef/trusted_certs
 knife configure -i
+
